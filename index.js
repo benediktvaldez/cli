1ae9cd3b18d67285850ae3a3ce3a1c710dbe3a7b
Benedikt D Valdez <benediktvaldez@gmail.com>
2017-08-28 17:30:21 +0000

    Move logic into lib

--- index.js
+++ index.js
@@ -1,31 +1,4 @@
 #!/usr/bin/env node
 'use strict'
-const meow = require('meow')
-const ora = require('ora')
 
-const b = require('./lib')
-const { start, succeed, fail } = b.helpers
-
-const cli = meow({
-  help: `
-    Usage
-      $ b <command> <input>
-
-    Commands
-      at         Update slack status to express where I'm at
-  `,
-})
-
-start()
-
-switch (cli.input[0]) {
-  case 'at':
-    b
-      .at(cli.input, cli.flags)
-      .then(status => succeed(`New status: ${status.status_text}`))
-      .catch(err => fail(err))
-    break
-  default:
-    cli.showHelp()
-    break
-}
+module.exports = require('./lib')
